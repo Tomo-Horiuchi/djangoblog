8cb2eb8403ee0b3b07e79cb5374abe8128439430
Tomo-Horiuchi <horiuchi.github@gmail.com>
2021-01-11 20:14:33 +0900

    first commit

---
+++ config/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.schemas import get_schema_view  # 追記
+from django.views.generic import TemplateView  # 追記
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/posts/', include('posts.urls')),
+    path('api/users/', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/rest-auth/', include('rest_auth.urls')),
+    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('schema/', get_schema_view(  # スキーマ表示の追加
+        title="Blogress",
+        description="API for all things …"
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(  # ドキュメント表示の追加
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
+]
